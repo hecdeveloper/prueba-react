2bc91af1ff20838102de391b1d34a782be2acc14
Hector <hibarra@imaginuity.com>
2023-08-07 21:28:43 -0500

    renderizado inicial

--- prueba-react/src/App.jsx
+++ prueba-react/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import responseMovies from "./mocks/results-response.json";
+import noResults from "./mocks/noresults.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="page">
+        <header>
+          <form className="form">
+            <label>
+              <input placeholder="Avengers, Starwars, The Matrix" />
+              <button type="submit">Buscar</button>
+            </label>
+          </form>
+        </header>
+        <main>
+          {hasMovies ? (
+            <ul>
+              {movies.map((movie) => (
+                <li key={movie.Title}>
+                  <h3>{movie.Title}</h3>
+                  <p>
+                    {movie.Year}
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No se encontraron peliculas</p>
+          )}
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
