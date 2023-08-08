5ad9e1e38617d4b92ad01e26d8591710776d7c3c
Hector <hibarra@imaginuity.com>
2023-08-07 22:11:53 -0500

    refactoriza componente

--- prueba-react/src/App.jsx
+++ prueba-react/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import responseMovies from "./mocks/results-response.json";
 import noResults from "./mocks/noresults.json";
+import { ListOfMovies } from "./components/Movies";
 
 function App() {
   const movies = responseMovies.Search;
-  const hasMovies = movies?.length > 0;
   return (
     <>
       <div className="page">
@@ -17,21 +17,7 @@ function App() {
           </form>
         </header>
         <main>
-          {hasMovies ? (
-            <ul>
-              {movies.map((movie) => (
-                <li key={movie.Title}>
-                  <h3>{movie.Title}</h3>
-                  <p>
-                    {movie.Year}
-                    <img src={movie.Poster} alt={movie.Title} />
-                  </p>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No se encontraron peliculas</p>
-          )}
+          <ListOfMovies movies={movies}/>
         </main>
       </div>
     </>
